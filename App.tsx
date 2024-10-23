b1b17325f1bcd0e5bdb9c581d6414e6127527eba
Abhishek <abhishek@gkmit.co>
2024-10-23 16:32:04 +0530

    feat: project setup - add `react-navigation` for drawer, stack and bottom tabs navigation - add icons - add deep-linking setup - add necessary packages

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function App(): React.JSX.Element {
+  return <Text>App</Text>;
+}
+
+export default App;
---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@screens': './src/screens',
+          '@utility': './src/utility',
+          '@components': './src/components',
+          '@contexts': './src/contexts',
+          '@navigation': './src/navigation',
+          '@network': './src/network',
+          '@storage': './src/storage',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+};
