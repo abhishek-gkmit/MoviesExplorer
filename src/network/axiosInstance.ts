ae4c7529687dfd0f7273d21969fca68bfa4834d8
Abhishek <abhishek@gkmit.co>
2024-10-28 14:29:45 +0530

    fix: add suggested changes

--- src/network/axiosInstance.ts
+++ src/network/axiosInstance.ts
@@ -1,19 +1,14 @@
 import axios from 'axios';
 
-const API_VERSION = 3;
-
-const ACCESS_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OWNlNzI5ZGNjYjc1Nzk4MTU5YmQ1NTc1MjUzNTI0MiIsIm5iZiI6MTcyOTc0NjY2NC42NzEyNjUsInN1YiI6IjY3MTljZjRkNWJlOWU4NzU5ZGE2ZGQxYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CRaTvR-R67HVxts401F1CIIbfbWKPlJjRQyHjbUw6ME';
-
-const API = axios.create({
-  baseURL: `https://api.themoviedb.org/${API_VERSION}/`,
+const axiosInstance = axios.create({
+  baseURL: `${process.env.API_BASE_URL}/${process.env.API_VERSION}/`,
   headers: {
-    Authorization: `Bearer ${ACCESS_TOKEN}`,
+    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
     Accept: 'application/json',
   },
 });
 
-API.interceptors.response.use(
+axiosInstance.interceptors.response.use(
   function onSuccess(response) {
     return response.data;
   },
@@ -23,4 +18,4 @@ API.interceptors.response.use(
   },
 );
 
-export default API;
+export default axiosInstance;
--- src/utility/dataFormatters.ts
+++ src/utility/dataFormatters.ts
@@ -1,10 +1,13 @@
-import { AxiosResponse } from 'axios';
-
 const ORIGINAL_IMAGE = 'https://image.tmdb.org/t/p/original';
 const W500_IMAGE = 'https://image.tmdb.org/t/p/w500';
 
 function formatMovies(data: any) {
   const results: RawMovieData[] = data?.results;
+
+  if (!results) {
+    return [];
+  }
+
   const formattedMovies: FormattedMovieData[] = results.map(function format({
     id,
     title,
@@ -28,6 +31,56 @@ function formatMovies(data: any) {
   return formattedMovies;
 }
 
+function getMoviesId(data: any) {
+  const results: RawMovieData[] = data?.results;
+
+  if (!results) {
+    return [];
+  }
+
+  const formattedMovies: string[] = results.map(function format({ id }) {
+    return id + '';
+  });
+
+  return formattedMovies;
+}
+
+function formatMovieInfo({
+  id,
+  title,
+  backdrop_path,
+  genres,
+  overview,
+  poster_path,
+  release_date,
+  runtime,
+  status,
+  vote_count,
+  vote_average,
+}: RawMovieInfo): FormattedMovieInfo {
+  return {
+    id,
+    title,
+    runtime,
+    status,
+    backdropUrl: W500_IMAGE + backdrop_path,
+    genres: [...genres],
+    plot: overview,
+    posterUrl: W500_IMAGE + poster_path,
+    releaseDate: release_date,
+    rating: vote_average,
+    ratingCount: vote_count,
+  };
+}
+
+function formatMovieImages(movieImages: Array<any>) {
+  movieImages = movieImages.slice(0, 10);
+
+  return movieImages.map(imageData => {
+    return W500_IMAGE + imageData?.file_path;
+  });
+}
+
 function trimText(text: string, length: number) {
   if (text.length <= length) {
     return text;
@@ -36,4 +89,10 @@ function trimText(text: string, length: number) {
   return text.slice(0, length);
 }
 
-export { formatMovies, trimText };
+export {
+  formatMovies,
+  formatMovieInfo,
+  formatMovieImages,
+  trimText,
+  getMoviesId,
+};
